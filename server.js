2f9d13656fcc620b61b9baf729cb155df5b3605a
Kamil Khan <kamil.z.khan@gmail.com>
2016-08-21 20:22:02 -0700

    added token validation

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ firebase.initializeApp({
   databaseURL: 'https://bread-e6858.firebaseio.com'
 });
 
-var firebaseDB = firebase.database();
-
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
@@ -30,54 +28,28 @@ app.post('/create_transaction', jsonParser, function(request, response) {
     if (!request.body) return response.sendStatus(400);
     var transactionID = uuid.v4();
     var userID        = "";
-    var userIDTest    = request.body.user_id;
     var clientID      = request.body.client_id;
-    var item          = request.body.item;
+    var itemID        = request.body.item_id;
     var quantity      = request.body.quantity;
     var bread         = request.body.bread;
-    // var token         = request.idToken;
-    //
-    // firebase.auth().verifyIdToken(token).then(function(decodedToken) {
-    //   userID = decodedToken.uid;
-    // }).catch (function (error) {
-    //   return response.sendStatus(400);
-    // });
+    var token         = request.body.user_token;
+
+    firebase.auth().verifyIdToken(token).then(function(decodedToken) {
+      userID = decodedToken.uid;
+    }).catch (function (error) {
+        console.log(error);
+        return response.sendStatus(400);
+    });
 
   var queryText = 'INSERT INTO public.pending_transactions VALUES ($1, $2, $3, $4, $5, $6)';
 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query(queryText, [transactionID, userIDTest, clientID, item, quantity, bread], function (err, result) {
-      if (err) console.log(err);
+    client.query(queryText, [transactionID, userID, clientID, itemID, quantity, bread], function (err, result) {
+      if (err) response.sendStatus(500);
       else {
-          monitorTransaction(userIDTest);
-        // return transaction ID to user (who will record it themselves in Firebase) and place monitor on uid's "transaction history" for 3 mins
-        // if it appears in "approved transactions", take steps to make it go through
-        // if it appears in "declined transactions", take steps for it to fail
+          response.status(200).json({transaction_id : transactionID});
       }
     });
   });
 
-  response.sendStatus(200);
 });
-
-function monitorTransaction(uid) {
-    var path = "users/" + uid + "/transactions/pending_transactions";
-    var ref = firebaseDB.ref(path);
-    console.log("monitoring user response at path: " + path);
-    function transactionCallback(snapshot, prevChildKey) {
-        var transactionID = snapshot.val();
-        executeTransaction(transactionID, uid);
-    }
-    ref.once("child_changed", transactionCallback);
-}
-
-function executeTransaction(transactionID, uid) {
-    console.log("CALLBACK CALLED!");
-    var ref = firebaseDB.ref("users/" + uid + "/pantry/balance");
-    pg.connect(process.env.DATA_URL, function(err, client, done) {
-        client.query('SELECT * FROM pending_transactions WHERE transaction_id = $1', [transactionID], function (err, result) {
-            if (err) throw err;
-            console.log(result.rows[0]);
-        })
-    });
-}
