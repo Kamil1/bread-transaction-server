f5b17f2ac41b6ffb860c6ccecf321f0dd3f708f3
Kamil Khan <kamil.z.khan@gmail.com>
2016-08-23 17:45:21 -0700

    added rate limiting for requests, improved error message responses, laid out execute_transaction endpoint

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.listen(port, function() {
 });
 
 app.post('/create_transaction', jsonParser, function(request, response) {
-    if (!request.body) return response.sendStatus(400);
+    if (!request.body) return response.status(400).send("Bad Request");
     var transactionID = uuid.v4();
     var userID        = "";
     var clientID      = request.body.client_id;
@@ -41,15 +41,39 @@ app.post('/create_transaction', jsonParser, function(request, response) {
         return response.status(401).send("Unauthorized");
     });
 
+    var countPendingTransactions = "SELECT COUNT(*) AS pending_transactions FROM public.pending_transactions WHERE userID = $1 AND created + INTERVAL '1 minute' >= NOW()";
     var queryText = 'INSERT INTO public.pending_transactions VALUES ($1, $2, $3, $4, $5, $6)';
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query(queryText, [transactionID, userID, clientID, itemID, quantity, bread], function (err, result) {
-            if (err) response.sendStatus(500);
-            else {
-                response.status(200).json({transaction_id : transactionID});
-            }
+        client.query(countPendingTransactions, [clientID], function(err, result) {
+            if (err) return response.status(500).send("Internal Server Error");
+            if (results.rows[0].pending_transactions > 3) return response.status(429).send("Too Many Requests");
+        });
+        client.query(queryText, [transactionID, userID, clientID, itemID, quantity, bread], function(err, result) {
+            if (err) return response.status(500).send("Internal Server Error");
+            response.status(200).json({transaction_id : transactionID});
         });
     });
+});
 
+app.post('/execute_transaction', jsonParser, function(request, response) {
+    if (!request.body) return response.status(400).send("Bad Request");
+    var transactionID = request.body.transaction_id;
+
+    var queryText = 'SELECT * FROM public.pending_transactions WHERE transaction_id = $1';
+
+    pg.connect(process.env.DATA_URL, function(err, client, done) {
+        client.query(queryText,  [transactionID], function(err, result) {
+            if (err) return reponse.status(500).send("Internal Server Error");
+            var row = resuls.rows[0];
+            var clientID = row.client_id;
+            var item = row.item_id;
+            var quantity = row.quantity;
+            var bread = row.bread;
+            var created = row.created;
+
+            // check to see if transaction is expired
+            // check if user is "locked" -- if so, place listener and wait until
+        })
+    })
 });
