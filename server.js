27dc5f4e72130bc325997aebc9cf9798fb19820f
Kamil Khan <kamil.z.khan@gmail.com>
2016-08-25 23:20:59 -0700

    all responses are now in json format

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ firebase.initializeApp({
   databaseURL: 'https://bread-e6858.firebaseio.com'
 });
 
+var poolConfig = {
+    max: 10
+};
+var pool = new pg.Pool(poolConfig);
+
+function getUserIdFrom(token) {
+    firebase.auth().verifyIdToken(token).then(function(decodedToken) {
+        userID = decodedToken.uid;
+        return userID;
+    }).catch (function (error) {
+        console.log(error);
+        return null;
+    });
+}
+
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
@@ -25,7 +40,7 @@ app.listen(port, function() {
 });
 
 app.post('/create_transaction', jsonParser, function(request, response) {
-    if (!request.body) return response.status(400).send("Bad Request");
+    if (!request.body) return response.status(400).json({error: "Bad Request"});
     var transactionID = uuid.v4();
     var userID        = "";
     var clientID      = request.body.client_id;
@@ -33,56 +48,105 @@ app.post('/create_transaction', jsonParser, function(request, response) {
     var quantity      = request.body.quantity;
     var bread         = request.body.bread;
     var token         = request.body.user_token;
-
-    function authorized() {
-        firebase.auth().verifyIdToken(token).then(function(decodedToken) {
-            userID = decodedToken.uid;
-            return true;
-        }).catch (function (error) {
-            console.log(error);
-            return false;
-        });
-    }
+    userID        = getUserIdFrom(token);
 
     function setupTransaction() {
-        pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        pool.connect(process.env.DATABASE_URL, function (err, client, done) {
             var countPendingTransactions = "SELECT COUNT(*) AS pending_transactions FROM public.pending_transactions WHERE user_id = $1 AND created + 90 >= EXTRACT(EPOCH FROM NOW())";
-            var queryText = 'INSERT INTO public.pending_transactions VALUES ($1, $2, $3, $4, $5, $6)';
-
             client.query(countPendingTransactions, [clientID], function (err, result) {
-                if (err) return response.status(500).send("Internal Server Error");
-                if (result.rows[0].pending_transactions > 3) return response.status(429).send("Too Many Requests");
-            });
-            client.query(queryText, [transactionID, userID, clientID, itemID, quantity, bread], function (err, result) {
-                if (err) return response.status(500).send("Internal Server Error");
-                return response.status(200).json({transaction_id: transactionID});
+                done();
+
+                if (err) {
+                    response.status(500).json({error: "Internal Server Error"});
+                    return;
+                }
+                if (result.rows[0].pending_transactions > 3) {
+                    response.status(429).json({error: "Too Many Requests"});
+                    return;
+                }
+                createPendingTransaction()
             });
         });
     }
 
-    if (authorized()) {
-        setupTransaction()
+    function createPendingTransaction() {
+        pool.connect(process.env.DATABASE_URL, function (err, client, done) {
+            var insertPendingTransaction = 'INSERT INTO public.pending_transactions VALUES ($1, $2, $3, $4, $5, $6)';
+            client.query(insertPendingTransaction, [transactionID, userID, clientID, itemID, quantity, bread], function (err) {
+                done();
+
+                if (err) {
+                    response.status(500).json({error: "Internal Server Error"});
+                    return;
+                }
+                response.status(200).json({transaction_id: transactionID});
+            });
+        });
+    }
+
+    if (userID === null) {
+        response.status(401).json({error: "Unauthorized"});
     } else {
-        return response.status(401).send("Unauthorized");
+        setupTransaction();
     }
 
 });
 
 app.post('/execute_transaction', jsonParser, function(request, response) {
-    if (!request.body) return response.status(400).send("Bad Request");
+    if (!request.body) {
+        response.status(400).json({error: "Bad Request"});
+        return;
+    }
     var transactionID = request.body.transaction_id;
+    var token         = request.body.user_token;
+    var tokenUserID   = getUserIdFrom(token);
 
-    var queryText = 'SELECT * FROM public.pending_transactions WHERE transaction_id = $1';
+    function invoiceTransaction() {
+        pool.connect(process.env.DATA_URL, function(err, client, done) {
+            var insertTransaction = 'INSERT INTO public.transactions SELECT transaction_id, user_id, client_id, item_id, quantity, bread FROM pending_transactions WHERE transaction_id = $1';
+            client.query(insertTransaction, [transactionID], function(err) {
+                done();
 
-    pg.connect(process.env.DATA_URL, function(err, client, done) {
-        client.query(queryText,  [transactionID], function(err, result) {
-            if (err) return reponse.status(500).send("Internal Server Error");
-            var row = resuls.rows[0];
+                if (err) throw err;
+                deletePendingTransaction();
+            })
+        })
+    }
+
+    function deletePendingTransaction() {
+        pool.connect(process.env.DATA_URL, function(err, client, done) {
+            var deletePendingTransaction = 'DELETE FROM public.transaction WHERE transaction_id = $1';
+            client.query(deletePendingTransaction, [transactionID], function(err) {
+                done();
+
+                if (err) throw err;
+            })
+        })
+    }
+
+    if (tokenUserID === null) {
+        response.status(401).json({error: "Unauthorized"});
+        return;
+    }
+
+    pool.connect(process.env.DATA_URL, function(err, client, done) {
+        var selectTransaction = 'SELECT * FROM public.pending_transactions WHERE transaction_id = $1';
+        client.query(selectTransaction, [transactionID], function(err, result) {
+            done();
+
+            if (err) {
+                response.status(500).json({error: "Internal Server Error"});
+                return;
+            }
+            var row = result.rows[0];
 
             var now = Date.now().getTime() / 1000;
             var created_datetime = row.created_datetime;
 
-            if (now - created_datetime > 90) return response.status(410).send("Gone: Transaction Expired");
+            if (now - created_datetime > 90) {
+                response.status(410).json({error: "Gone: Transaction Expired"});
+                return;
+            }
 
             var clientID = row.client_id;
             var item = row.item_id;
@@ -90,18 +154,35 @@ app.post('/execute_transaction', jsonParser, function(request, response) {
             var bread = row.bread;
             var userID = row.user_id;
 
-            var pantry = firebase.database().ref('users/' + userID + 'pantry/bread_balance');
-            pantry.transaction(function(currentBalance) {
+            if (userID != tokenUserID) {
+                response.status(401).json({error: "Unauthorized"});
+                return;
+            }
+
+            var userPantry = firebase.database().ref('users/' + userID + '/pantry/bread_balance');
+            userPantry.transaction(function(currentBalance) {
                 if (currentBalance < bread) return;
                 return currentBalance - bread;
-            }, function(error, committed, snapshot) {
+            }, function(error, committed) {
                 if (error) {
-                    return response.status(500).send
+                    response.status(500).json({error: "Internal Server Error"});
+                } else if (!committed) {
+                    response.status(200).json({result: "Insufficient Funds"});
+                } else {
+                    var userItem = firebase.database().ref('users/' + userID + '/clients/' + clientID + '/' + item);
+                    userItem.transaction(function(currentItem) {
+                        if (currentItem === null) return quantity;
+                        return currentItem + quantity;
+                    }, function(error) {
+                        if (error) {
+                            response.status(500).json({error: "Internal Server Error"});
+                        } else {
+                            invoiceTransaction();
+                            response.status(200).json({result: "Transaction Completed Successfully"});
+                        }
+                    })
                 }
             });
-
-
-            // check if user is "locked" -- if so, place listener and wait until
         })
     })
 });
